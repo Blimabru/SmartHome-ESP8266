4786a89069c414508962e56734b67c09ab1534f3
Bruno Lima <bru.no@outlook.com.br>
2024-05-27 10:13:40 -0300

    Criação do arquivo onde é inserido os SSIDs e senhas das redes Wi-Fi

---
+++ credenciais.ino
@@ -0,0 +1,14 @@
+void credenciaisWifi() {
+
+  // Cadastrar múltiplas redes Wi-Fi
+  wifiMulti.addAP("SSID", "Senha");
+  wifiMulti.addAP("SSID", "Senha");
+  wifiMulti.addAP("SSID", "Senha");
+  wifiMulti.addAP("SSID", "Senha");
+  wifiMulti.addAP("SSID", "Senha");
+  wifiMulti.addAP("SSID", "Senha");
+  
+}
+
+// URL para a API que faz conexão com o Banco de Dados:
+String url = "http://www.dominio.com/api/esp8266/conexao";
