be9ecef61180ffeabf5f999c564b72b91feeca67
Blimabru <bru.no@outlook.com.br>
2024-05-29 01:43:57 -0300

    Refatoração na forma em que os dados são enviados para o servidor Web

--- conexaoSite.ino
+++ conexaoSite.ino
@@ -1,90 +1,40 @@
-#include <ESP8266HTTPClient.h>  //https://arduino.esp8266.com/stable/package_esp8266com_index.json
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 
 extern String url;
 
-void enviaRequisicaoPOST(String sensor, String valor) {
-
-  if (WiFi.status() == WL_CONNECTED) {  // Verifica se está conectado ao WiFi
-    HTTPClient http;                    // Cria uma instância de HTTPClient
-    WiFiClient client;                  // Cria uma instância de WiFiClient
-
-    http.begin(client, url);  // Especifica o URL do servidor usando WiFiClient
-
-    // Adiciona cabeçalhos
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-
-    // Dados para enviar via POST
-    String parametros = "sensor=" + sensor + "&valor=" + valor;
-
-    // Envia requisição POST
-    int httpResponseCode = http.POST(parametros);
-
-    // Verifica o código de resposta
-    if (httpResponseCode > 0) {
-
-      String response = http.getString();  // Obter a resposta do servidor
-
-      //Serial.println("Enviando dados para o servidor:");
-      Serial.print("Sensor: ");
-      //Serial.println(httpResponseCode);   // Imprimir código de resposta
-      Serial.println(response);  // Imprimir resposta do servidor
-      delay(150);
-      
-    } else {
-
-      Serial.print("Erro ao enviar dados para o servidor Web: ");
-      Serial.println(httpResponseCode);
-    }
-
-    http.end();  // Finaliza a requisição
-  } else {
-    Serial.println("Erro na conexão WiFi");  // Imprime mensagem de erro de conexão
+void enviarRequisicaoPOST(String dados, const char* operacao) {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Erro na conexão WiFi");
+    return;
   }
+
+  HTTPClient http;
+  WiFiClient client;
+
+  http.begin(client, url);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  int httpResponseCode = http.POST(dados);
+
+  if ((httpResponseCode > 0) && (operacao != "e")) {
+    String response = http.getString();
+    enviarDadosArduino(response.c_str());
+
+  } else {
+    Serial.print("Erro na requisição HTTP: ");
+    Serial.println(httpResponseCode);
+  }
+
+  http.end();
 }
 
-void verificarEstadoDispositivo(String dispositivo) {
-  if (WiFi.status() == WL_CONNECTED) {  // Verifica se está conectado ao WiFi
-    HTTPClient http;                    // Cria uma instância de HTTPClient
-    WiFiClient client;                  // Cria uma instância de WiFiClient
+void verificarEstadoDispositivos(String dispositivo) {
+  String dados = "dispositivo=" + dispositivo;
+  enviarRequisicaoPOST(dados, "v");
+}
 
-    http.begin(client, url);                             // Especifica o URL do servidor usando WiFiClient
-    http.addHeader("Content-Type", "application/json");  // Adiciona cabeçalho
-
-    // Define o payload JSON a ser enviado
-    String jsonPayload = "{\"dispositivo\":\"" + dispositivo + "\"}";
-    // Envia a requisição POST
-    int httpResponseCode = http.POST(jsonPayload);
-
-    if (httpResponseCode > 0) {            // Verifica o código de resposta
-      String response = http.getString();  // Obtém a resposta do servidor
-
-      //Serial.print("Dispositivo: ");
-      //Serial.println(httpResponseCode); // Imprime o código de resposta
-      //Serial.println(response);  // Imprime a resposta do servidor
-
-      // Remove os caracteres de início e fim: { }
-      response.remove(0, 2);                     // Remove os dois primeiros caracteres ' { " '
-      response.remove(response.length() - 2);  // Remove os doi últimos caracteres ' " } " '
-
-      // Encontra o índice do separador ':'
-      int separatorIndex = response.indexOf(':');
-
-      // Extrai o nome do dispositivo e o estado, removendo também as aspas extras
-      String dispositivo = response.substring(0, separatorIndex);  // Remove aspas no início e no fim do dispositivo
-      dispositivo.remove(dispositivo.length() - 1);
-      String estado = response.substring(separatorIndex + 2);  // Remove aspas no início e no fim do estado
-
-      enviaEstadoDispositivoArduino(dispositivo, estado);
-      delay(150);
-
-    } else {
-      Serial.print("Erro ao baixar dados do servidor Web: ");  // Imprime mensagem de erro
-      Serial.println(httpResponseCode);                        // Imprime o código de resposta
-      delay(150);
-    }
-
-    http.end();  // Finaliza a requisição
-  } else {
-    Serial.println("Erro na conexão WiFi");  // Imprime mensagem de erro de conexão
-  }
+void enviarDadosSensor(String sensor, String valor) {
+  String dados = "sensor=" + sensor + "&valor=" + valor;
+  enviarRequisicaoPOST(dados, "e");
 }
